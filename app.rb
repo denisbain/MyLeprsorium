5604e73a5277bfa0def94bda7794aa9983435a9e
Denis Bain <baindenis95@gmail.com>
2024-02-04 08:40:21 -0300

    add gem sqlite3 and def init_db

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+  db = SQLite3::Database.new(leprosorium2.db)
+  db.results_as_hash = true
+end
 
 configure do
   enable :sessions
